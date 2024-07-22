2c06af92f18b18ea0c7b24f73e375d8e43d442cc
David <david.sarrut@gmail.com>
2024-07-22 15:07:43 +0200

    test003 data will be remote

--- gaga_phsp/gaga_helpers_tests.py
+++ gaga_phsp/gaga_helpers_tests.py
@@ -1,4 +1,3 @@
-
 import os
 import inspect
 import colored
@@ -17,7 +16,6 @@ except AttributeError:
     color_ok = colored.fore("green")
 
 
-
 def fatal(s):
     caller = inspect.getframeinfo(inspect.stack()[1][0])
     ss = f"(in {caller.filename} line {caller.lineno})"
@@ -26,6 +24,7 @@ def fatal(s):
     s = colored.stylize(s, color_error)
     print(s)
     raise Exception(s)
+
 
 def run_and_check(cmd):
     print()
@@ -47,19 +46,20 @@ def test_ok(is_ok=False):
         print(s)
         sys.exit(-1)
 
+
 def compare_sampled_points(keys, real, fake, wtol=0.1, tol=0.08):
     for i in range(len(keys)):
-        w = scipy.stats.wasserstein_distance(real[:,i], fake[:,i])
+        w = scipy.stats.wasserstein_distance(real[:, i], fake[:, i])
         print(f"({i}) Key {keys[i]}, wass = {w:.2f}  tol = {wtol:.2f}")
         if w > wtol:
             fatal(f"Difference between real and fake too large {w} vs {wtol}")
-        real_mean = np.mean(real[:,i])
-        real_std = np.std(real[:,i])
-        fake_mean = np.mean(fake[:,i])
-        fake_std = np.std(fake[:,i])
+        real_mean = np.mean(real[:, i])
+        real_std = np.std(real[:, i])
+        fake_mean = np.mean(fake[:, i])
+        fake_std = np.std(fake[:, i])
         d_mean = np.fabs((real_mean - fake_mean) / real_mean)
         d_std = np.fabs((real_std - fake_std) / real_std)
-        print(f"({i}) Mean real vs fake : {real_mean:.2f} {fake_mean:.2f} {d_mean*100:.2f}%")
-        print(f"({i}) Std real vs fake : {real_std:.2f} {fake_std:.2f} {d_std*100:.2f}%")
+        print(f"({i}) Mean real vs fake : {real_mean:.2f} {fake_mean:.2f} {d_mean * 100:.2f}%")
+        print(f"({i}) Std real vs fake : {real_std:.2f} {fake_std:.2f} {d_std * 100:.2f}%")
         if d_mean > tol or d_std > tol:
             fatal(f"Difference between real and fake too large {d_mean} {d_std} vs {tol}")
---
+++ tests/test003_param.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import gaga_phsp as gaga
+from gatetools.phsp import load
+import numpy as np
+
+if __name__ == "__main__":
+    """
+    Convert ideal pos to exit pos
+    """
+
+    # input
+    dataset_filename = f"data/test003/spect_training_dataset.root"
+    npy1_filename = f"output/test003_exit_pos.npy"
+    npy2_filename = f"output/test003_ideal_pos.npy"
+    plot_filename = f"output/test003.png"
+
+    # step 1
+    cmd = f"gaga_exit_pos_to_ideal_pos {dataset_filename} -n 1e4 -o {npy1_filename}"
+    gaga.run_and_check(cmd)
+
+    # step 2
+    cmd = f"gaga_ideal_pos_to_exit_pos {npy1_filename} -n 1e4 -o {npy2_filename}"
+    gaga.run_and_check(cmd)
+
+    # step 3
+    cmd = f"gt_phsp_plot {npy2_filename} {dataset_filename} -n 1e4 -o {plot_filename}"
+    gaga.run_and_check(cmd)
+    print(f"Results in {plot_filename}")
+
+    # compare
+    data1, read_keys1, m1 = load(dataset_filename, "phase_space")
+    data2, read_keys2, m2 = load(npy2_filename)
+
+    keys = ['KineticEnergy',
+            'PrePosition_X', 'PrePosition_Y', 'PrePosition_Z',
+            'PreDirection_X', 'PreDirection_Y', 'PreDirection_Z',
+            'TimeFromBeginOfEvent',
+            'EventPosition_X', 'EventPosition_Y', 'EventPosition_Z',
+            'EventDirection_X', 'EventDirection_Y', 'EventDirection_Z']
+    tol = 0.01
+    is_ok = True
+    for k in keys:
+        index = read_keys1.index(k)
+        x1 = data1[:, index]
+        index = read_keys2.index(k)
+        x2 = data2[:, index]
+        m1 = np.mean(x1)
+        m2 = np.mean(x2)
+        d = abs(m1 - m2) / m1
+        b = d < tol
+        print(f"{k} = {m1:.3f} +/- {d:.3f} < {tol:.3f} : {b}")
+        is_ok = is_ok and b
+
+    # end
+    gaga.test_ok(True)
